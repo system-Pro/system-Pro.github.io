33a16c1f0e917e6608919a2d4e2e31f0c3a29703
Send <sendmyletterto@gmail.com>
2016-10-08 14:37:30 +0300

    js home8_2 перемещение табами

--- JS_HOME8/2/js/script.js
+++ JS_HOME8/2/js/script.js
@@ -1,10 +1,12 @@
 ﻿$(function () {
+
     var $label = $('.myform label');
+    var labelPosition = 1;     //перемещение tab
     var $tooltip = $('.tooltip');
     var $button = $('input[type=button]');
+    var buttonPosition = false;
     $label.mouseover(function () {
         var id = '#' + ($(this).attr('data-tooltip'));
-        console.log(id);
         $(id).stop().fadeIn('fast');
     });
     $label.mouseout(function () {
@@ -12,6 +14,30 @@
         $(id).stop().fadeOut('fast');
     });
     $button.on('click', function () {
-        $tooltip.toggle('fast');
+        if (buttonPosition == false) {
+            $tooltip.stop().fadeIn('fast');
+            buttonPosition = true;
+        }
+        else {
+            $tooltip.stop().fadeOut('fast');
+            buttonPosition = false;
+        }
     });
+
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode == 9) {
+            e.preventDefault();
+            if (labelPosition > $label.length) {
+                labelPosition = 1;
+            }
+            $('.inp' + labelPosition).focus();
+            if (labelPosition == 1) {
+                $('#' + $label.length).stop().fadeOut('fast');
+            }
+            $('#' + (labelPosition-1)).stop().fadeOut('fast');
+            $('#' + labelPosition).stop().fadeIn('fast');
+            labelPosition++;
+        }
+    })
+    
 });
