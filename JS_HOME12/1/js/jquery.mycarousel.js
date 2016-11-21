a846084bd5430fddcb27301e7cc3c0de5bcda55c
Anton Shtokalo <anton.shtokalo@gmail.com>
2016-11-21 19:51:39 +0200

    js_home 12

---
+++ JS_HOME12/1/js/jquery.mycarousel.js
@@ -0,0 +1,37 @@
+﻿(function ($) {
+    $.fn.mycarousel = function () {
+        var $carousel = $(this);
+        var $left = $carousel.find('.carousel__left');
+        var $right = $carousel.find('.carousel__right');
+        var $list = $carousel.find('.carousel__list');
+        var $liCount = $list.find('.carousel__element').length;
+
+        var pixelsOffset = 125;
+        var currentLeftValue = 0;
+
+        var minOffset = -(($liCount - 5) * pixelsOffset);
+        var maxOffset = 0;
+
+        $left.click(moveLeft);
+        $right.click(moveRight);
+        console.log("start: "+ currentLeftValue);
+
+        function moveLeft() {
+            if (currentLeftValue != maxOffset) {
+                currentLeftValue += 125;
+                $list.animate({ left: currentLeftValue + 'px' }, 500);
+                console.log("left: " + currentLeftValue);
+            }
+        }
+
+        function moveRight() {
+            if (currentLeftValue != minOffset) {
+                currentLeftValue -= 125;
+                $list.animate({ left: currentLeftValue + 'px' }, 500);
+                console.log("right: " + currentLeftValue);
+            }
+        }
+        
+        return this;
+    }
+})(jQuery);
---
+++ JS_HOME12/2/js/script.js
@@ -0,0 +1,27 @@
+﻿$(function () {
+
+    var html = $('#template').html();
+    var student = [
+        {
+            name: 'Ivanov Ivan',
+            age: '27', 
+            phone: '066'
+        },
+        {
+            name: 'Sergeev Sergey',
+            age: '22',
+            phone: '063'
+        },
+        {
+            name: 'Andreev Andrey',
+            age: '25',
+            phone: '067'
+        }
+    ];
+
+    var group = tmpl(html, {
+        data: student
+    });
+
+    $('body').append(group);
+});
