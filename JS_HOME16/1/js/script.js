3e6c5a9845ecd8325b2b5382bc95007078106aef
Anton Shtokalo <anton.shtokalo@gmail.com>
2016-12-19 19:11:45 +0200

    js_16

---
+++ JS_HOME16/1/js/script.js
@@ -0,0 +1,38 @@
+﻿;
+$(function () {
+    function getPhoto() {
+        var word = $('.text-field').val();
+        var counter = counter ? counter : 4;
+        var loader = $('.loader');
+        $.ajax({
+            url: 'https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=' + word + '&limit=' + counter,
+            dataType: 'json',
+            beforeSend: function () {
+                loader.show(0);
+            },
+            complete: function () {
+                loader.delay(2000).hide(400);
+            },
+            success: function (data, textStatus, xhr) {
+                console.log(data, textStatus, xhr.status);
+                var $img = $('.image');
+                $img.children().remove();
+                if (data.results.length == 0) {
+                    $img.append('<p>Nothing found</p>');
+                }
+                for (var i = 0; i < data.results.length; i++) {
+                    var imgSrc = data.results[i].url;
+                    $img.append('<img src="' + imgSrc + '" alt="img' + [i + 1] + '" />');
+                }
+            },
+        });
+    };
+
+    $('.mbtn').click(getPhoto);
+    window.addEventListener('keydown', function (e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            getPhoto();
+        }
+    })
+});
---
+++ JS_HOME16/2/js/script.js
@@ -0,0 +1,60 @@
+﻿;
+$(function () {
+    'use strict';
+
+    function Human(name, age, gender, height, weight) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+    }
+    Human.prototype.show = function () {
+        console.log('name: ' + this.name);
+        console.log('age: ' + this.age);
+        console.log('gender: ' + this.gender);
+        console.log('height: ' + this.height);
+        console.log('weight: ' + this.weight);
+    }
+
+    function Worker(name, age, gender, height, weight, jobAddr, salary) {
+        this.jobAddr = jobAddr;
+        this.salary = salary;
+        Human.apply(this, arguments);
+    }
+    Worker.prototype = Object.create(Human.prototype);
+    Worker.prototype.constructor = Worker;
+    Worker.prototype.show = function () {
+        Human.prototype.show.apply(this);
+        console.log('jobAddr: ' + this.jobAddr);
+        console.log('salary: ' + this.salary);
+    };
+    Worker.prototype.work = function () {
+        console.log('Work work!');
+    }
+
+    function Student(name, age, gender, height, weight, stydyPlace, scholarship) {
+        this.stydyPlace = stydyPlace;
+        this.scholarship = scholarship;
+        Human.apply(this, arguments);
+    }
+    Student.prototype = Object.create(Human.prototype);
+    Student.prototype.constructor = Student;
+    Student.prototype.show = function () {
+        Human.prototype.show.apply(this);
+        console.log('stydyPlace: ' + this.stydyPlace);
+        console.log('scholarship: ' + this.scholarship);
+    };
+    Student.prototype.learn = function () {
+        console.log('Learn learn!');
+    }
+
+    var wor = new Worker('Worker', 27, 'male', 190, 70, 'Verbitskogo', 500);
+    wor.show();
+    wor.work();
+    console.log(' ');
+
+    var stu = new Student('Student', 20, 'male', 180, 60, 'street', 100);
+    stu.show();
+    stu.learn();
+});
