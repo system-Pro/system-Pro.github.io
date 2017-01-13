888cb5f972dc5c8884c33cc75770fe2a1c16708d
systemPro <sendmyletterto@gmail.com>
2017-01-13 02:31:35 +0200

    js_home20 part

--- JS_HOME20/js/main.js
+++ JS_HOME20/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
-
+	// $('.jcarousel').jcarousel({
+ //        // Core configuration goes here
+ //        wrap: 'circular'
+ //    })
+ //    .jcarouselAutoscroll({
+ //        interval: 7000,
+ //        target: '+=1',
+ //        autostart: true
+ //    });
 });
