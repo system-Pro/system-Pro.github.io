04cee2020999535383dbc5cc04cab44d21ac2025
systemPro <sendmyletterto@gmail.com>
2017-01-13 02:36:03 +0200

    js_home20 part

--- JS_HOME20/js/main.js
+++ JS_HOME20/js/main.js
@@ -1,11 +1,11 @@
 $(function(){
-	// $('.jcarousel').jcarousel({
- //        // Core configuration goes here
- //        wrap: 'circular'
- //    })
- //    .jcarouselAutoscroll({
- //        interval: 7000,
- //        target: '+=1',
- //        autostart: true
- //    });
+	 $('.jcarousel').jcarousel({
+         // Core configuration goes here
+         wrap: 'circular'
+     })
+     .jcarouselAutoscroll({
+         interval: 7000,
+         target: '+=1',
+         autostart: true
+     });
 });
